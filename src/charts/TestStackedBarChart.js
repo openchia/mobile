6c9c443b0af7720b4423e9ce520067736f52838d
Ronald Goedeke <ronald.goedeke@outlook.com>
2021-11-19 12:47:37 +0100

    Working stackedbarchart

--- src/charts/TestStackedBarChart.js
+++ src/charts/TestStackedBarChart.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import React from 'react';
 import { useWindowDimensions, View } from 'react-native';
 import * as shape from 'd3-shape';
@@ -5,60 +6,94 @@ import * as array from 'd3-array';
 import * as scale from 'd3-scale';
 import { Text } from 'react-native-paper';
 import { Svg, Rect, Path } from 'react-native-svg';
+import Animated, { useAnimatedProps, useSharedValue } from 'react-native-reanimated';
+import { mixPath } from 'react-native-redash';
+
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 const TestStackedBarChart = ({ data, height, keys, colors }) => {
   const { width } = useWindowDimensions();
+  const spacingInner = 0.05;
+  const spacingOuter = 0.05;
+  const previous = useSharedValue(0);
+  const current = useSharedValue(0);
+  const transition = useSharedValue(0);
 
-  const series = shape.stack().keys(keys).order(shape.stackOrderNone).offset(shape.stackOffsetNone)(
-    data
-  );
-
-  const xAccessor = ({ index }) => index;
-
-  // double merge arrays to extract just the yValues
-  const yValues = array.merge(array.merge(series));
-  const xValues = data.map((item, index) => xAccessor({ item, index }));
-
-  const yExtent = array.extent([...yValues, 0, height]);
-  const xExtent = array.extent(xValues);
-
-  const yMin = yExtent[0];
-  const yMax = yExtent[1];
-  const xMin = xExtent[0];
-  const xMax = xExtent[1];
-
-  const y = scale.scaleLinear().domain([yMin, yMax]).range([height, 0]).clamp(true);
-
-  const x = scale.scaleLinear().domain([xMin, xMax]).range([0, width]).clamp(true);
-
-  const areas = series.map((serie, index) => {
-    const path = shape
-      .area()
-      .x((d, index) => x(xAccessor({ item: d.data, index })))
-      .y0((d) => y(d[0]))
-      .y1((d) => y(d[1]))
-      .curve(shape.curveLinear)(data.map((_, index) => serie[index]));
-
+  const animatedProps = useAnimatedProps(() => {
+    const previousPath = graphs[previous.value].data.path;
+    const currentPath = graphs[current.value].data.path;
     return {
-      path,
-      key: keys[index],
-      color: colors[index],
+      d: mixPath(transition.value, previousPath, currentPath),
     };
   });
+
+  const valueAccessor = ({ item, key }) => item[key];
+
+  const series = shape
+    .stack()
+    .keys(keys)
+    .order(shape.stackOrderNone)
+    .value((item, key) => valueAccessor({ item, key }))
+    .offset(shape.stackOffsetNone)(data);
+
+  const values = array.merge(array.merge(series));
+  const indexes = values.map((_, index) => index);
+
+  const extent = array.extent([...values, 0, height]);
+
+  const y = scale.scaleLinear().domain(extent).range([height, 0]);
+  const x = scale
+    .scaleBand()
+    .domain(data.map((_, index) => index))
+    .range([0, width])
+    .paddingInner([spacingInner])
+    .paddingOuter([spacingOuter]);
+
+  const bandwidth = x.bandwidth();
+
+  const areas = array.merge(
+    series.map((serie, keyIndex) =>
+      serie.map((entry, entryIndex) => {
+        const path = shape
+          .area()
+          .y0((d) => y(d[0]))
+          .y1((d) => y(d[1]))
+          .x((d, _index) => (_index === 0 ? x(entryIndex) : x(entryIndex) + x.bandwidth()))
+          .defined((d) => !isNaN(d[0]) && !isNaN(d[1]))([entry, entry]);
+
+        return {
+          path,
+          color: colors[keyIndex],
+          key: keys[keyIndex],
+        };
+      })
+    )
+  );
+
   return (
     <View height={height}>
       <Svg height={height} width={width}>
-        {areas.map((area, index) => (
-          <Path
-            key={area.key}
-            // fill="red"
-            fill={area.color}
-            // {...svgs[index]}
-            // animate={animate}
-            // animationDuration={animationDuration}
-            d={area.path}
-          />
-        ))}
+        {areas.map((bar, index) => {
+          const keyIndex = index % data.length;
+          const key = `${keyIndex}-${bar.key}`;
+          const { svg } = data[keyIndex][bar.key];
+
+          return (
+            <AnimatedPath
+              animatedProps={animatedProps}
+              {...svg}
+              fill={bar.color}
+              // stroke="black"
+              // strokeWidth={3}
+            />
+            // <Path
+            //   key={key}
+            //   fill={bar.color}
+            //   {...svg}
+            //   d={bar.path}
+            // />
+          );
+        })}
       </Svg>
     </View>
   );
