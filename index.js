86eb9f527b9bad4bdc2234f6ee5f28ae72d17183
Ronald Goedeke <ronald.goedeke@outlook.com>
2021-11-22 20:07:44 +0100

    Change reanimated 2 version

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 import 'react-native-gesture-handler';
 // import 'react-native-reanimated';
 import { AppRegistry } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
-import { Notifications } from 'react-native-notifications';
 import App from './App';
 import { name as appName } from './app.json';
 
