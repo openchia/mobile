978638ceb68ab6b420fd792d7cce0d35c710e269
Ronald Goedeke <ronald.goedeke@outlook.com>
2021-11-23 01:02:12 +0100

    small change

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -127,7 +127,7 @@ const Content = ({ navigation }) => {
             refreshing={refreshing}
             onRefresh={() => {
               setRefreshing(true);
-              refresh();
+              // refresh();
             }}
           />
         }
