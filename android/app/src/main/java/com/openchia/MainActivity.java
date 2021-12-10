fddc3d63b542fc82187c530caafc4a7fc07d94c2
Ronald Goedeke <ronald.goedeke@outlook.com>
2021-12-10 22:17:56 +0100

    fixed crash

--- android/app/src/main/java/com/openchia/MainActivity.java
+++ android/app/src/main/java/com/openchia/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this, R.style.SplashScreenTheme);
+    SplashScreen.show(this, R.style.SplashScreenTheme, false);
     super.onCreate(null);
   }
 }
