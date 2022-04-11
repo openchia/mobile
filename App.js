896808fa03cc1bb358b2b4e833586aa294434207
BubbleTrouble14 <ronald.goedeke@outlook.com>
2022-04-11 16:16:18 +0200

    added missing changes

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import messaging from '@react-native-firebase/messaging';
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Platform } from 'react-native';
 import { Notifications } from 'react-native-notifications';
+// import { enableFreeze } from 'react-native-screens';
 import { RecoilRoot } from 'recoil';
-import ApplicationNavigator from './src/ApplicationNavigator';
-import LoadingComponent from './src/components/LoadingComponent';
-// import './src/constants/IMLocalize';
+import BaseScreen from './src/BaseScreen';
 import './src/localization/i18n';
 
 // enableFreeze(true);
@@ -18,34 +17,6 @@ if (Platform.OS === 'android') {
   require('intl/locale-data/jsonp/en-US');
   require('intl/locale-data/jsonp/en-IN'); // load the required locale details
 }
-
-// const updateLocalization = () => {
-//   Crowdin.initWithHashString('fcaf0dd82870a8dfbcc5f9876j9', DEFAULT_LANGUAGE, (message) => {});
-
-//   Crowdin.getResourcesByLocale('uk', (data) => {
-//     var response = JSON.parse(data);
-
-//     translations.setContent(
-//       Object.assign({}, translations.getContent(), {
-//         uk: response.strings,
-//       })
-//     );
-
-//     this.resetState();
-//   });
-// };
-
-const requestUserPermission = async () => {
-  // const token = await messaging().getToken();
-  const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  if (enabled) {
-    // console.log('Authorization status:', authStatus);
-  }
-};
 
 const onMessageReceived = (message) => {
   Notifications.postLocalNotification({
